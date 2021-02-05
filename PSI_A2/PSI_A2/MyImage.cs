9d1d6f8bc3f12554d8e370e10d38557b78217bd1
mat <matthieu2603@gmail.com>
2021-02-05 15:57:49 +0100

    Ajout class MyImage

--- PSI_A2/PSI_A2/MyImage.cs
+++ PSI_A2/PSI_A2/MyImage.cs
@@ -8,5 +8,23 @@ namespace PSI_A2
 {
     class MyImage
     {
+        private string type;
+        private int taille;
+        private int offset;
+        private int width;
+        private int height;
+        private int bits_by_Color;
+        private int[,] image;
+
+        public MyImage(string type, int taille, int offset, int width, int height, int bits_by_Color, int[,] image)
+        {
+            this.type = type;
+            this.taille = taille;
+            this.offset = offset;
+            this.width = width;
+            this.height = height;
+            this.bits_by_Color = bits_by_Color;
+            this.image = image;
+        }
     }
 }
