67f07c942f100f425a36b679885fd0fdecb476cd
mat <matthieu2603@gmail.com>
2021-02-05 17:15:21 +0100

    Ajout constructeur class MyImage

--- PSI_A2/PSI_A2/MyImage.cs
+++ PSI_A2/PSI_A2/MyImage.cs
@@ -16,7 +16,7 @@ namespace PSI_A2
         private int width;
         private int height;
         private int bits_by_Color;
-        private int[,] image;
+        private byte[,] image;
 
         public MyImage(string type, int taille, int offset, int width, int height, int bits_by_Color, int[,] image)
         {
@@ -40,10 +40,10 @@ namespace PSI_A2
                 
                 this.taille = Convertir_Endian_To_Int(ByteToInt(image_temp,2,4));
                 this.offset = Convertir_Endian_To_Int(ByteToInt(image_temp, 10, 4));
-                this.width = Convertir_Endian_To_Int(ByteToInt(image_temp, 18, 4));
-                this.height = Convertir_Endian_To_Int(ByteToInt(image_temp, 22, 4));
+                this.width = Convertir_Endian_To_Int(ByteToInt(image_temp, 18, 4))*3;
+                this.height = Convertir_Endian_To_Int(ByteToInt(image_temp, 22, 4))*3;
                 this.bits_by_Color = Convertir_Endian_To_Int(ByteToInt(image_temp, 28, 2));
-                this.image = new int[this.height, this.width];
+                this.image = new byte[this.height, this.width];
                 for (int i = this.offset; i < this.image.Length; i+=this.height)
                 {
                     for(int j = i; j < this.width; j++)
@@ -57,6 +57,30 @@ namespace PSI_A2
 
             }
         }
+
+        public void FromImageToFile(string file)
+        {
+            if(this.type == "BM")
+            {
+                byte[] image_to_write = new byte[this.offset + (this.height * this.width)];
+                int counter = 0;
+
+
+
+
+                for(int i = 0; i < this.image.GetLength(0); i++)
+                {
+                    for(int j= 0;j< this.image.GetLength(1); j++)
+                    {
+                        image_to_write[this.offset + counter] = this.image[i, j];
+                        counter++;
+                    }
+                }
+
+            }
+                
+        }
+
         static int ByteToInt(byte[] tab,int pos, int length)
         {
             string retour = "";
