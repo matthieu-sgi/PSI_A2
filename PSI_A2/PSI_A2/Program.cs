d3d050fc7f587b5d3073982ec5485ab59e0a7002
sn00wden <matthieu2603@gmail.com>
2021-04-13 12:27:43 +0200

    Modif for qrcode and legibility

--- PSI_A2/PSI_A2/Program.cs
+++ PSI_A2/PSI_A2/Program.cs
@@ -10,45 +10,7 @@ namespace PSI_A2
     {
         static void Main(string[] args)
         {
-            #region
-            /*//Bitmap damier = new Bitmap(@"..\..\..\Images\damier.bmp");
-
-            byte[] file = File.ReadAllBytes(@"..\..\..\Images\damier.bmp");
-            Console.WriteLine("\n HEADER \n");
-            for (int i = 0; i < 14; i++)
-            {
-                Console.Write(file[i] + " ");
-            }
-            Console.WriteLine("\n HEADER INFO \n");
-
-            for (int i = 14; i < 54; i++)
-            {
-                Console.Write(file[i] + " ");
-
-            }
-
-
-
-
-
-            Console.WriteLine("\n IMAGE \n");
-
-            for (int i = 54; i < file.Length; i += 60)
-            {
-                for (int j = i; j < i + 60; j++)
-                {
-                    Console.WriteLine(file[j] + " ");
-                }
-                Console.WriteLine();
-            }
-
-
-
-            //damier.Save(@"..\..\..\Images\Tests_damier.bmp");
-            //Equivalent de la save de bitmap
-            //File.WriteAllBytes(@"..\..\..\Images\Tests_damier.bmp",file);
-            Console.ReadKey();*/
-            #endregion
+            
 
             string path = @"..\..\..\Images\tigre.bmp";
             string writing_path = @"..\..\..\Images\test.bmp";
